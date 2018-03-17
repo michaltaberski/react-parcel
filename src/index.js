fed914ca56c2141c07383c60cb9ae3a0442b0d93
michaltaberski <m.taberski@gmail.com>
2018-03-17 21:44:33 +0100

    Simple design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import { Flex, Box } from 'grid-styled';
+import 'antd/dist/antd.css';
+import './style.css';
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+import { Layout, Input, Button } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+const HeaderBody = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const ContentBody = styled.div`
+  background: #fff;
+  margin: 16px auto;
+  padding: 16px;
+  max-width: 1200px;
+`;
+
+const FooterBody = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 0.8em;
+`;
+
+ReactDOM.render(
+  <Layout>
+    <Header>
+      <HeaderBody>
+        <h1>Title</h1>
+      </HeaderBody>
+    </Header>
+    <Content style={{ padding: '16px' }}>
+      <ContentBody>
+        <Flex align="center" justify="center">
+          <h2>Login</h2>
+        </Flex>
+        <Flex align="center" justify="center" flexDirection="column">
+          <Box mb={16}>
+            <Input size="large" />
+          </Box>
+          <Box mb={16}>
+            <Input size="large" type="password" />
+          </Box>
+          <Box>
+            <Button type="primary">Login</Button>
+          </Box>
+        </Flex>
+      </ContentBody>
+    </Content>
+    <Footer>
+      <FooterBody>
+        Demo by <a href="http://michaltaberski.com">Michał Taberski</a>
+      </FooterBody>
+    </Footer>
+  </Layout>,
+  document.getElementById('root')
+);
